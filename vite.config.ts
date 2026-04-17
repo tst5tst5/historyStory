4478cd86de3b76b69af13e62025f6367e35ac9e4
AA-Dev <aa-dev@workbuddy.ai>
2026-04-17 20:15:26 +0800

    deploy: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/historyStory/',
   server: {
     host: '0.0.0.0',
     allowedHosts: true,
